a47c2660287f5c7de34cc56dadcb37d5902756e4
Zain <expendables891@gmail.com>
2024-06-08 15:57:54 +0500

    Data format parsing added

--- save-csv.php
+++ save-csv.php
@@ -1,14 +1,28 @@
 <?php
 include 'db-connection.php';
 
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: https://departures.summertimes.com.cy');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
 
+
+function parseDate($dateStr) {
+    $formats = ['d-M-y', 'd M Y', 'd/M/Y'];
+    foreach ($formats as $format) {
+        $dateObj = DateTime::createFromFormat($format, $dateStr);
+        if ($dateObj !== false) {
+            return $dateObj;
+        }
+    }
+    return false; // Return false if none of the formats match
+}
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] == UPLOAD_ERR_OK) {
     // Check if the file type is CSV
     $fileType = mime_content_type($_FILES['csv_file']['tmp_name']);
+    echo $fileType;
     if ($fileType == 'text/csv') {
         // Open uploaded CSV file with read-only mode
         $csvFile = fopen($_FILES['csv_file']['tmp_name'], 'r');
@@ -23,14 +37,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['csv_file']) && $_FILE
 
         // Parse data from CSV file line by line
         while (($line = fgetcsv($csvFile)) !== FALSE) {
-            // Get row data
-            $departureDate = DateTime::createFromFormat('d-M-y', $line[0])->format('Y-m-d');
+            // Trim and validate date strings
+            $departureDateStr = trim($line[0]);
+            $pickupDateStr = trim($line[6]);
+
+            // Convert date strings to Y-m-d format
+            $departureDateObj = parseDate($departureDateStr);
+            $pickupDateObj = parseDate($pickupDateStr);
+
+            if ($departureDateObj !== false && $pickupDateObj !== false) {
+                $departureDate = $departureDateObj->format('Y-m-d');
+                $pickupDate = $pickupDateObj->format('Y-m-d');
+            } else {
+                // Handle date parsing error
+                fclose($csvFile);
+                http_response_code(400); // Bad Request
+                echo json_encode(["success" => false, "message" => "Invalid date format in CSV file."]);
+                exit;
+            }
+
             $bookingNumber = $line[1];
             $toName = $line[2];
             $flightNumber = $line[3];
             $flightDepTime = $line[4];
             $pickUpTime = $line[5];
-            $pickupDate = DateTime::createFromFormat('d-M-y', $line[6])->format('Y-m-d');
             $hotel = $line[7];
             $pickupPoint = $line[8];
             $serviceType = $line[9];
